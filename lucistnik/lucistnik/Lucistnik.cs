3cddb60e05692be2f3939d949d72a2745e59d8e1
Jana Strážnovská <jana.straznovska@gmail.com>
2025-04-24 23:13:45 +0200

    Zdrojove kody domaciho ukolu Lucistnik

---
+++ lucistnik/lucistnik/Lucistnik.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UkolLucisnik
+{
+    public class Lucistnik
+    {
+        string _jmeno;
+        int _pocetSipu;
+        public Lucistnik(string jmeno, int pocetSipu)
+        {
+            _jmeno = jmeno;
+            _pocetSipu = pocetSipu;
+        }
+
+        public void Vystrel()
+        {
+            if (_pocetSipu > 0)
+            {
+                Console.WriteLine ("Vystrel byl uspesny.");
+                _pocetSipu--;
+            }
+            else
+            {
+                Console.WriteLine ("Nemas dostatek sipu.");
+            }
+        }
+
+        public void PrijedSipy(int pocet)
+        {
+            if (pocet > 0)
+            {
+                _pocetSipu = _pocetSipu + pocet;
+                Console.WriteLine ($"Pocet sipu je {_pocetSipu}");
+            }
+        }
+
+        public void ZobrazStav()
+        {
+            Console.WriteLine ($"Aktualni jmeno lucistnika je {_jmeno} a ma {_pocetSipu} sipu.");
+        }
+
+    }
+}
---
+++ lucistnik/lucistnik/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UkolLucisnik;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Lucistnik Pepa = new Lucistnik("Pepa", 6);
+
+        while (true)
+        {
+            Pepa.ZobrazStav();
+            Console.WriteLine("1. Vystrelit sip");
+            Console.WriteLine("2. Pridat sipy");
+            Console.WriteLine("3. Konec");
+
+            string hodnotaCoZadalUzivatel = Console.ReadLine();
+            int coMaLucistnikDelat;
+            bool spravnaHodnota = int.TryParse(hodnotaCoZadalUzivatel, out coMaLucistnikDelat);
+
+            if (spravnaHodnota = false)
+            {
+                Console.WriteLine($"Nezadal jsi cislo, zadej hodnotu znovu.");
+            }
+            else
+            {
+                switch (coMaLucistnikDelat)
+                {
+                    case 1:
+                        Pepa.Vystrel();
+                        break;
+                    case 2:
+                        int pocetPridavanychSipu = NactiCeleCisloZKonzole("Zadej pocet sipu");
+                        
+                        Pepa.PrijedSipy(pocetPridavanychSipu);
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Nezadal jsi platnou volbu.");
+                        break;
+                }
+
+            }
+
+
+        }
+
+    }
+
+    public static int NactiCeleCisloZKonzole(string vyzva)
+    {
+
+
+        while (true)
+        {
+            Console.WriteLine(vyzva);
+            string zadanaHodnotaOdUzivatele = Console.ReadLine();
+            int zadaneCisloOdUzivatele = 0;
+
+            bool jeToCislo = int.TryParse(zadanaHodnotaOdUzivatele, out zadaneCisloOdUzivatele);
+
+            if (jeToCislo && (zadaneCisloOdUzivatele > 0))
+            {
+                return zadaneCisloOdUzivatele;
+            }
+        }
+    }
+}
