f2de6207d2f2aafd630d96a68bef5301af8a330d
Jana Strážnovská <jana.straznovska@gmail.com>
2025-05-15 17:16:19 +0200

    Domací úkol - plánovač událostí

---
+++ domaciUkolPlanovacUdalosti/planovacUdalosti/Event.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace planovacUdalosti
+{
+    public class Event
+    {
+        string eventName;
+        DateTime eventDate;
+
+        public Event(string name, DateTime date)
+        {
+            eventName = name;
+            eventDate = date;
+        }
+
+        public void Print()
+        {
+            TimeSpan timeToEvent = eventDate - DateTime.Now;
+            if (timeToEvent.Days > 0)
+            {
+                Console.WriteLine($"Event {eventName} with date {eventDate:yyyy-MM-dd} will happen in {timeToEvent.Days} days.");
+            }
+            else
+            {
+                Console.WriteLine($"Event {eventName} with date {eventDate:yyyy-MM-dd} happened {-timeToEvent.Days} days ago.");
+            }
+            
+        }
+    }
+}
---
+++ domaciUkolPlanovacUdalosti/planovacUdalosti/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+
+namespace planovacUdalosti;
+
+class Program
+{
+    static List<Event> eventsList = new List<Event>();
+    static Dictionary<DateTime, int> eventsDictionary = new Dictionary<DateTime, int>();
+
+
+    static void Main(string[] args)
+    {
+        //Tests();
+        //return;
+
+        while (true)
+        {
+            Console.WriteLine("Enter data:");
+            string data = Console.ReadLine();
+            if (!ProcessData(data))
+            {
+                return;
+            }
+        }
+    }
+
+    private static void Tests()
+    {
+        ProcessData("EVENT;Lekce Czechitas;2025-05-17");
+        ProcessData("EVENT;Oslava;2025-05-10");
+        ProcessData("EVENT;Odevzdat úlohu;2025-04-20");
+        ProcessData("EVENT;Vyšetření;2025-05-10");
+        ProcessData("LIST");
+        ProcessData("STATS");
+        ProcessData("END");
+
+        ProcessData("EVENT;Vyšetření,2025-05-10");
+        ProcessData("EVENT Vyšetření 2025-05-10");
+        ProcessData("TEST");
+
+    }
+
+
+    private static bool ProcessData(string inputString)
+    {
+        if (inputString.StartsWith("EVENT"))
+        {
+            string[] parts = inputString.Split(";");
+
+            //for (int i = 0; i < parts.Length; i++)
+            //{
+            //    Console.WriteLine($"[{i}] = {parts[i]}");
+            //}
+
+            if (parts.Length == 3)
+            {
+                string dateInputString = parts[2];
+
+                DateTime dateInput = DateTime.ParseExact(dateInputString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+                Event newEvent = new Event(parts[1], dateInput);
+
+                eventsList.Add(newEvent);
+
+                if (eventsDictionary.ContainsKey(dateInput))
+                {
+                    eventsDictionary[dateInput]++;
+                }
+                else
+                {
+                    eventsDictionary.Add(dateInput, 1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data error!");
+            }
+        }
+
+        else if (inputString.StartsWith("LIST"))
+        {
+            foreach (var e in eventsList)
+            {
+                e.Print();
+            }
+        }
+
+        else if (inputString.StartsWith("STATS"))
+        {
+            foreach (DateTime d in eventsDictionary.Keys)
+            {
+                Console.WriteLine($"Date: {d:yyyy-MM-dd} Events: {eventsDictionary[d]}");
+            }
+
+        }
+        else if (inputString.StartsWith("END"))
+        {
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Command error!");
+        }
+
+        return true;
+
+    }
+
+}
